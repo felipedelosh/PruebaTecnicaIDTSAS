4a2b52cc60e641aca2515bbe9a0b53e983f35169
felipedelosh <doctorfhernandez@hotmail.com>
2023-07-07 19:01:48 -0500

    Test 02

---
+++ 01 - Básico/ejercicio02/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Pagina</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="ejemplo plantilla HTML"/>
+  <meta name="keywords" content="palabra clave 1, palabra clave 2, palabra clave 3"/>
+  <meta name="author" content="FelipedelosH" />
+  <meta name="copyright" content="FelipedelosH" />
+  <meta name="robots" content="/"/>
+</head>
+
+<body>
+  <h1>Ejercicio 2: JavaScript Basics</h1>
+  <p>Escribe una función en JavaScript que reciba un arreglo de números como entrada y devuelva la suma de los números pares en el arreglo.</p>
+  <br>
+  <p>Nota: ingrese los numeros separados por ","</p>
+  <br>
+  <input type="text" id="arrayNumbers" name="arrayNumbers" required minlength="10" maxlength="200" size="50">
+  <button onclick="sumVector()">SUMAR!!!</button> 
+  <hr>
+  <p id="result">?</p>
+
+  <script src="js/javascript.js"></script>
+</body>
+</html>
---
+++ 01 - Básico/ejercicio02/js/javascript.js
@@ -0,0 +1,53 @@
+/**
+ * This method read HTML and Stract numbers array
+ */
+function sumVector(){ 
+    let textArr = document.getElementById("arrayNumbers").value;   
+    data = getFormatedArray(textArr);
+
+    if(data.length > 0){
+        var sum = sumOnlyPairsInVector(data);
+        document.getElementById("result").innerHTML = sum;
+    }else{
+        alert("Error Fatal");
+    }
+
+}  
+
+
+/**
+ * 
+ * @param {*} data is a text and hav numbers sepatates via ,
+ * @returns [#, #, ...#]
+ */
+function getFormatedArray(data){
+    try {
+        data = data.split(",");
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+      
+    return [];
+}
+
+/**
+ * 
+ * @param {*} array Enter a vector [str#, str#, str#, str#]
+ * @returns 
+ */
+function sumOnlyPairsInVector(array){
+    let sum = 0;
+
+    try {
+        for (let i = 0; i < array.length; i++) {
+            if (parseInt(array[i])%2 == 0){
+                sum = sum + parseInt(array[i]);
+            }
+        } 
+    } catch (error) {
+        console.log(error);
+    }
+
+    return sum;
+}
