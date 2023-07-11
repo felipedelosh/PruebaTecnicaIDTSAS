e9612116f8fa914183ce2bad37ddc2798389ebf0
felipedelosh <doctorfhernandez@hotmail.com>
2023-07-11 14:39:07 -0500

    refactorin test 04

---
+++ 01 - Básico/ejercicio04/includes/banner.php
@@ -0,0 +1,5 @@
+<h1>Ejercicio 4: JavaScript + DOM Manipulation</h1>
+<p>Crea una página HTML con un botón. Escribe una función en JavaScript que, al hacer clic en el botón, cambie el color de fondo de la página al azar</p>
+<hr>
+<button onclick="changeColorBG()">COLOR!!!</button>
+<script src="js/javascript.js"></script>
--- 01 - Básico/ejercicio04/js/javascript.js
+++ 01 - Básico/ejercicio04/js/javascript.js
@@ -1,11 +1,20 @@
+/**
+ * READ body and change the color to rnd method
+ */
 function changeColorBG(){
     var body = document.getElementsByTagName("body");
+    //get RBG color to rnd numbers (0, ,255)
     var red = getRNDColor();
     var green = getRNDColor();
     var blue = getRNDColor();
     body[0].style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
 }
 
+/**
+ * 
+ * @returns integer with scale rgb color (0-255)
+ * 
+ */
 function getRNDColor(){
     min = 0;
     max = 255;
